10b175fc83525f4d8a9e6a8b0938f0344b944dd7
Elvin Zeinalabdziyeu <elvinzeinalabdziyeu@gmail.com>
2020-08-21 15:36:46 +0300

    add toggle button

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,54 @@
-import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
+import React, { Component } from 'react';
+import { Col, Row, Container } from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
 
+import './app.css';
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+
+export default class App extends Component {
+
+	state = {
+		toggle : false
+	}
+
+	onToggle = () => {
+		this.setState({
+			toggle: !this.state.toggle
+		})
+	}
+
+	render() {
+
+		const {toggle} = this.state;
+
+		const randomChar = toggle ? null : <RandomChar />;
+		return (
+			<>
+				<Container>
+					<Header />
+				</Container>
+				<Container>
+					<Row>
+						<Col lg={{ size: 5, offset: 0 }}>
+							{randomChar}
+							<button
+								className='btn btn-dark'
+								onClick={this.onToggle}>Toggle random character</button>
+						</Col>
+					</Row>
+					<Row>
+						<Col md='6'>
+							<ItemList />
+						</Col>
+						<Col md='6'>
+							<CharDetails />
+						</Col>
+					</Row>
+				</Container>
+			</>
+		);
+	}
 };
-
-export default App;
