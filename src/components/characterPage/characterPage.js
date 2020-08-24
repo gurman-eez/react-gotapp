f90534dd00dd3258fe13555593b577d615595149
Elvin Zeinalabdziyeu <elvinzeinalabdziyeu@gmail.com>
2020-08-24 10:50:26 +0300

    separate charactorPage

--- src/components/characterPage/characterPage.js
+++ src/components/characterPage/characterPage.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Col, Row, Container } from 'reactstrap';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
 import ErrorMessage from '../errorMessage';
 import GotService from '../../services/gotService';
+import RowBlock from '../rowBlock';
 
 
 export default class CharacterPage extends Component {
@@ -28,6 +28,16 @@ export default class CharacterPage extends Component {
 	}
 
 	render() {
+		const itemList = (
+			<ItemList
+				onCharSelected={this.onCharSelected}
+				getData={this.gotService.getAllCharacters}
+				renderItem={({ name, gender }) => `${name} (${gender})`} />
+		)
+		const charDetails = (
+			<CharDetails
+				charId={selectedChar} />
+		)
 
 		const {error, selectedChar} = this.state;
 
@@ -36,18 +46,7 @@ export default class CharacterPage extends Component {
 		}
 
 		return (
-			<Row>
-				<Col md='6'>
-					<ItemList
-						onCharSelected={this.onCharSelected}
-						getData={this.gotService.getAllCharacters}
-						renderItem={({name, gender}) => `${name} (${gender})` } />
-				</Col>
-				<Col md='6'>
-					<CharDetails
-						charId={selectedChar} />
-				</Col>
-			</Row>
+			<RowBlock left={itemList} rigth={charDetails}/>
 		)
 	}
 }
